2861b4d218fbce9587b221266b2759dbef7086f5
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-01-27 21:33:44 +0900

    FIX: interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,12 +1,12 @@
-import { PoolConnection, Pool } from 'mysql';
+import { PoolConnection } from 'mysql';
 
 export abstract class IMySqlPoolConnection {
-  protected abstract readonly pool: Pool;
-  protected abstract readonly isDebug: boolean;
-  protected abstract async query<T = any>(connection: PoolConnection, queryStr: string): Promise<T>;
-  protected abstract async beginTransaction(connection: PoolConnection): Promise<void>;
-  protected abstract async commit(connection: PoolConnection): Promise<void>;
-  protected abstract async getConnection(): Promise<PoolConnection>;
+  // protected abstract readonly pool: Pool;
+  // protected abstract readonly isDebug: boolean;
+  // protected abstract async query<T = any>(connection: PoolConnection, queryStr: string): Promise<T>;
+  // protected abstract async beginTransaction(connection: PoolConnection): Promise<void>;
+  // protected abstract async commit(connection: PoolConnection): Promise<void>;
+  // protected abstract async getConnection(): Promise<PoolConnection>;
   abstract async executeReadOnly<T = any>(queryStr: string): Promise<T>;
   abstract async execute<T = any>(connection: PoolConnection, queryStr: string): Promise<T>;
   abstract async executeWithTransaction<T = any>(scope: (connection: PoolConnection) => Promise<T>): Promise<T>;
